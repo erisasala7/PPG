6f4970b1df1336d0b0db1ece923eb9d5226e45ad
Erisa Sala <erisasala7@gmail.com>
2021-03-19 13:26:56 +0100

    Done: -CODE FORMAT -GETESTET
    TODO:
    -BAT FILE MIT EIGENE BSP
    -UNIT TESTS
    
    PS_Client file gelöscht

---
+++ PlaylistGame/REST/Interfaces/Classes/Request.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PlaylistGame
+{
+    public class Request : IRequest
+    {
+        public const string MethodPost = "POST";
+        public const string MethodGet = "GET";
+        public const string MethodPut = "PUT";
+        public const string MethodDelete = "DELETE";
+        private readonly string _rawRequest;
+        public string Body;
+        public string ctype;
+        public object loc;
+        public string payload;
+        public string token;
+
+        public Request(string rawRequest)
+        {
+            _rawRequest = rawRequest;
+            SplitRawRequest();
+        }
+
+        public string Protocol { get; private set; }
+
+        public string UserAuthorization
+        {
+            get
+            {
+                if (Headers.ContainsKey("authorization")) return Headers["authorization"];
+                return "";
+            }
+        }
+
+        public bool IsValid
+        {
+            get
+            {
+                if (Method.Length != 0 && Url != null) return true;
+                return false;
+            }
+        }
+
+        public string Method { get; private set; }
+
+        public IUrl Url { get; private set; }
+
+        public IDictionary<string, string> Headers { get; private set; }
+
+        public string UserAgent
+        {
+            get
+            {
+                if (Headers.ContainsKey("user-agent")) return Headers["user-agent"];
+                return "";
+            }
+        }
+
+        public int HeaderCount => Headers.Count;
+
+        public int ContentLength
+        {
+            get
+            {
+                if (Headers.ContainsKey("content-length")) return int.Parse(Headers["content-length"]);
+
+                return 0;
+            }
+        }
+
+        public string ContentType
+        {
+            get
+            {
+                if (Headers.ContainsKey("content-type"))
+                    return Headers["content-type"];
+
+                return "";
+            }
+        }
+
+        public Stream ContentStream { get; private set; }
+
+        public string ContentString { get; private set; }
+
+        public byte[] ContentBytes { get; private set; }
+
+        private void SplitRawRequest()
+        {
+            var requestLines =
+                _rawRequest.Split(Environment
+                    .NewLine); //in this case we need to keep empty lines to know where the body starts
+            var bodyStartIndex =
+                Array.IndexOf(requestLines,
+                    string.Empty); //IndexOf returns first appearance --> first empty line marks start of body
+            var requestStart = requestLines[0];
+            var headerLines = new string[bodyStartIndex - 1];
+            var bodyLines = new string[requestLines.Length - bodyStartIndex - 1];
+
+            //copies the header part into the header array, and the body part in the body array skipping the empty line between
+            Array.Copy(requestLines, 1, headerLines, 0, bodyStartIndex - 1);
+            Array.Copy(requestLines, bodyStartIndex + 1, bodyLines, 0, requestLines.Length - bodyStartIndex - 1);
+
+            //extract method and url form the headers array (index 0)
+            var methodAndUrl = requestStart.Split(" ", 3);
+            if (methodAndUrl.Length < 3)
+                throw new InvalidDataException(
+                    "Method/Url Could not be Parsed!"); //something went wrong, request cannot be used
+
+            //store in the respective variables
+            string[] validMethods = {"GET", "POST", "PUT", "PATCH", "DELETE"}; //defines which methods are valid
+
+            Method = ((IList) validMethods).Contains(methodAndUrl[0]) ? methodAndUrl[0] : "";
+            Url = new Url(methodAndUrl[1]);
+            Protocol = methodAndUrl[2];
+            Headers = ExtractHeaders(headerLines);
+
+            if (!bodyLines[0].Equals(string.Empty)
+            ) //when no body is provided, the bodyLines array contains 1 element with an empty string
+            {
+                //get string
+                ContentString = string.Join(Environment.NewLine, bodyLines);
+
+                //get byte array from string:
+                ContentBytes = Encoding.UTF8.GetBytes(ContentString);
+
+                //get stream from byte array: 
+                ContentStream = new MemoryStream(ContentBytes);
+            }
+        }
+
+        private IDictionary<string, string> ExtractHeaders(string[] headers)
+        {
+            IDictionary<string, string> headerDict = new Dictionary<string, string>();
+
+            foreach (var header in headers)
+            {
+                var keyValue = header.Split(": ");
+                headerDict.Add(keyValue[0].ToLower(), keyValue[1]);
+            }
+
+            return headerDict; //if the input array is empty, we return an empty dictionary, which is okay
+        }
+    }
+}
--- PlaylistGame/REST/Interfaces/IRequest.cs
+++ PlaylistGame/REST/Interfaces/IRequest.cs
@@ -5,59 +5,59 @@ namespace PlaylistGame
 {
     public interface IRequest
     {
-        
         /// <summary>
-        /// Returns true if the request is valid. A request is valid, if method and url could be parsed. A header is not necessary.
+        ///     Returns true if the request is valid. A request is valid, if method and url could be parsed. A header is not
+        ///     necessary.
         /// </summary>
         bool IsValid { get; }
 
         /// <summary>
-        /// Returns the request method in UPPERCASE. get -> GET.
+        ///     Returns the request method in UPPERCASE. get -> GET.
         /// </summary>
         string Method { get; }
 
         /// <summary>
-        /// Returns a URL object of the request. Never returns null.
+        ///     Returns a URL object of the request. Never returns null.
         /// </summary>
         IUrl Url { get; }
-        
+
         /// <summary>
-        /// Returns the request header. Never returns null. All keys must be lower case.
+        ///     Returns the request header. Never returns null. All keys must be lower case.
         /// </summary>
         IDictionary<string, string> Headers { get; }
 
         /// <summary>
-        /// Returns the user agent from the request header
+        ///     Returns the user agent from the request header
         /// </summary>
         string UserAgent { get; }
 
         /// <summary>
-        /// Returns the number of header or 0, if no header where found.
+        ///     Returns the number of header or 0, if no header where found.
         /// </summary>
         int HeaderCount { get; }
 
         /// <summary>
-        /// Returns the parsed content length request header.
+        ///     Returns the parsed content length request header.
         /// </summary>
         int ContentLength { get; }
 
         /// <summary>
-        /// Returns the parsed content type request header. Never returns null.
+        ///     Returns the parsed content type request header. Never returns null.
         /// </summary>
         string ContentType { get; }
 
         /// <summary>
-        /// Returns the request content (body) stream or null if there is no content stream.
+        ///     Returns the request content (body) stream or null if there is no content stream.
         /// </summary>
         Stream ContentStream { get; }
 
         /// <summary>
-        /// Returns the request content (body) as string or null if there is no content.
+        ///     Returns the request content (body) as string or null if there is no content.
         /// </summary>
         string ContentString { get; }
 
         /// <summary>
-        /// Returns the request content (body) as byte[] or null if there is no content.
+        ///     Returns the request content (body) as byte[] or null if there is no content.
         /// </summary>
         byte[] ContentBytes { get; }
     }
